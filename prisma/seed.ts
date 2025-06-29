584d93e9892b96f7845162e4ccc255e5e8c1c045
alvihrhp <rafael.hrhp@yahoo.com>
2025-06-29 12:20:00 +0700

    fix bcryptjs in prisma seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, JenisMobil, EngineType, Role, BookingStatus } from '@prisma/client';
-import * as bcrypt from 'bcryptjs';
+const bcrypt = require('bcryptjs')
 
 const prisma = new PrismaClient();
 
